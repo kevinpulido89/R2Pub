f39c35886b4773caa1e954f193c3767103bed4b8
Kevin Pulido <kevinp8001@gmail.com>
2016-08-22 11:31:56 -0500

    Update R2Eon.py

--- R2Eon.py
+++ R2Eon.py
@@ -5,7 +5,7 @@ import logger
 
 GPIO = 27
 
-pubnub = Pubnub(publish_key='pub-c-cbfee07b-226d-46a2-a4e4-18827eb2552c',uuid='RasPI')
+pubnub = Pubnub(publish_key='pub-c-cbfee07b-226d-46a2-a4e4-18827eb2552c', subscribe_key='sub-c-12b25da4-5597-11e6-a5a4-0619f8945a4f', uuid='RasPI')
 
 my_channel = 'temp_humid'
 
