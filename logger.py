86e5bcafe74ebe8dd9943a264a4ab50ebd52de23
Kevin Pulido <kevinp8001@gmail.com>
2016-08-22 11:26:13 -0500

    Create logger.py

---
+++ logger.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+now = datetime.now()
+
+def log(t,h):
+    file = open('log.csv','a')
+    file.write("T[C];H[%];")
+    file.write(str(t) + ";" + str(h))
+    file.write('\n')
+    file.close()
+
+file = open('log.csv','a')
+file.write('%s/%s/%s;%s:%s:%s;' % (now.day, now.month, now.year, now.hour, now.minute, now.second) + "\n")
+file.close()
