632cd06e4ef8d8fb19adb9efacf714ab26d8a891
Kevin Pulido <kevinp8001@gmail.com>
2016-08-23 12:57:44 -0500

    Update PubNub2Raspberry.py

--- PubNub2Raspberry.py
+++ PubNub2Raspberry.py
@@ -1,20 +1,17 @@
 from pubnub import Pubnub
+import time
 
 pubnub = Pubnub(publish_key='pub-c-cbfee07b-226d-46a2-a4e4-18827eb2552c', subscribe_key='sub-c-12b25da4-5597-11e6-a5a4-0619f8945a4f')
 
 my_channel = 'temp_humid'
 
-def _callback(message, channel):
-    print(message)
-
-def _error(message):
-    print(message)
-
 def loop():
     While True:
-        s = pubnub.subscribe(my_channel, callback=_callback, error=_error)
-        #pubnub.history(my_channel, count=100, callback=_callback, error=_error)
+        s = pubnub.subscribe(my_channel)
+        h = #pubnub.history(my_channel, count=100, callback=_callback, error=_error)
         print(s)
+        #print(h)
+        time.sleep(2.628)
 
 def destroy():
     print("Goodbye")
