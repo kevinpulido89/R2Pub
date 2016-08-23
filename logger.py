7b95ebc44c9d0d121440c3d6a9a5516a131390f9
Kevin Pulido <kevinp8001@gmail.com>
2016-08-23 12:27:16 -0500

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 def log(t,h):
-    print("Hi!")
     now = datetime.now()
     file = open('log.csv','a')
     file.write('%s/%s/%s,%s:%s:%s,' % (now.day, now.month, now.year, now.hour, now.minute, now.second))
