590a42348dcb80f5eacd56ba8df2a1fe1ecb581a
Kevin Pulido <kevinp8001@gmail.com>
2016-10-19 15:28:37 -0500

    Cambios Importantes

--- PublishAll.py
+++ PublishAll.py
@@ -1,9 +1,9 @@
+#!/usr/bin/python3
 # Librerias
 import os
 import time
 import paho.mqtt.client as paho
 import Adafruit_DHT as dht
-from ubidots import ApiClient
 from urllib.parse import urlparse
 from pubnub import Pubnub
 from datetime import datetime
@@ -18,13 +18,6 @@ my_channel = 'temp_humid'
 
 # Crear paquete de llaves PUBNUB
 pubnub = Pubnub(publish_key='pub-c-cbfee07b-226d-46a2-a4e4-18827eb2552c', subscribe_key='sub-c-12b25da4-5597-11e6-a5a4-0619f8945a4f', uuid='RasPI')
-
-# Create an ApiClient object
-api = ApiClient(token='faApU0YBjSMIl3mBHDigaqs6bFvnR0')
-
-# Get a Ubidots Variable
-variableT = api.get_variable('57d97d357625424f07b976b3')
-variableH = api.get_variable('57d97d3b7625424fa22d3595')
 
 # Define event callbacks and Functions
 def on_connect(mosq, obj, rc):
@@ -52,7 +45,7 @@ def Setup():
         time.sleep(0.5)
     return h,t
 
-def log(t,h):
+def log(h,t):
     now = datetime.now()
     file = open('log.csv','a')
     file.write('%s/%s/%s,%s:%s:%s,' % (now.day, now.month, now.year, now.hour, now.minute, now.second))
@@ -60,27 +53,29 @@ def log(t,h):
     file.write('\n')
     file.close()
 
-def loop():
+def loop(h,t):
     while True:
         ph=sum(h)/len(h)
         pt=sum(t)/len(t)
-        #pt += 6
+        #pt *= 1.43
         pubnub.publish(my_channel, {
-            "eon":{"Temperatura [C]":pt,"Humedad [%]":ph}})
+            "eon":{"Temperatura":pt,"Humedad":ph}})
+        print ('Temperature={0:0.1f}*C Humidity={1:0.1f}%'.format(pt,ph))
+        
+        # Almacena datos en log Local
+        log(ph,pt)
+
+        # Publish a message mqtt
+        mqttc.publish("Temp/value", pt)
+        mqttc.publish("Humid/value", ph)
+		
+	# Sobre-escribe array t y h
         t.remove(t[0])
         h.remove(h[0])
         h_temp,t_temp = dht.read_retry(dht.DHT11, GPIO)
         t.insert(len(t),t_temp)
         h.insert(len(h),h_temp)
-        print ('Temperature={0:0.1f}*C Humidity={1:0.1f}%'.format(pt,ph))
-        log(pt,ph)
-                # Write the value to your variable in Ubidots
-        responseT = variableT.save_value({"value": pt})
-        responseH = variableH.save_value({"value": ph})
-        #print (response)
-        # Publish a message mqtt
-        mqttc.publish("Temp/value", pt)
-        mqttc.publish("Humid/value", ph)
+
         time.sleep(5)
 
 def destroy():
@@ -108,6 +103,6 @@ mqttc.connect(serverMosquitto, portMosquitto)
 if __name__ == "__main__":
     h,t = Setup()
     try:
-        loop()
+        loop(h,t)
     except KeyboardInterrupt:
         destroy()
--- logger.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-def log(t,h):
-    now = datetime.now()
-    file = open('log.csv','a')
-    file.write('%s/%s/%s,%s:%s:%s,' % (now.day, now.month, now.year, now.hour, now.minute, now.second))
-    file.write(str(t) + "," + str(h))
-    file.write('\n')
-    file.close()
